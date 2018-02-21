f789c9de4fd51002294d253c96c446993134685d
dk87 <daniel.krefft@tum.de>
2018-02-21 14:14:26 +0100

    fix for coreapi object

--- src/app/mbl_client/mbl_client.cc
+++ src/app/mbl_client/mbl_client.cc
@@ -8,8 +8,9 @@
 
 mbl_client::mbl_client(const char* id) : mosquittopp(id)
 {
-        /* initialize roboteq core api */
-        CoreAPI * _core = new CoreAPI(CONNECTION_COM);
+    /* initialize roboteq core api */
+	CoreAPI c_api(CONNECTION_COM);
+    CoreAPI * _core = &c_api;
 
 	mosqpp::lib_init();  /* initialize mosquitto library */
 
@@ -70,8 +71,7 @@ void mbl_client::on_message(const struct mosquitto_message *message)
 
 	if (!strcmp(type, "powerpct")) {
 		unsigned int powerpct = atoi(value);
-		/* TODO set motor power percentage to powerpct */
-                _core->SetMotorSpeedAbs(powerpct);
+        _core->SetMotorSpeedAbs(powerpct);
 	} else {
 		Genode::log("unknown topic: ", (const char *)message->topic);
 	}
